14011230c68c784cce53b9a852852e4ba766e945
Miguel Martheyn <MiguelMartheyn@hotmail.com>
2020-11-09 00:56:07 -0500

    Updates

---
+++ precache-manifest.e46d3826671909d598c4de24a56a750a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7de50ae5875ff3f5266444362f9bdfc9",
+    "url": "/MiguelMartheynNewsRoom/index.html"
+  },
+  {
+    "revision": "6d8e4faa866ee88c0a55",
+    "url": "/MiguelMartheynNewsRoom/static/css/main.805cd8d7.chunk.css"
+  },
+  {
+    "revision": "21f021fa4198bce85bff",
+    "url": "/MiguelMartheynNewsRoom/static/js/2.19eef7c0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/MiguelMartheynNewsRoom/static/js/2.19eef7c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d8e4faa866ee88c0a55",
+    "url": "/MiguelMartheynNewsRoom/static/js/main.26e314f5.chunk.js"
+  },
+  {
+    "revision": "fa6501f50d8aaf459f76",
+    "url": "/MiguelMartheynNewsRoom/static/js/runtime-main.206ea734.js"
+  },
+  {
+    "revision": "779781883952778b25974baa3cf7679c",
+    "url": "/MiguelMartheynNewsRoom/static/media/cargando.77978188.gif"
+  }
+]);
